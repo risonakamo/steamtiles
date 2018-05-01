f3291eec6868b139dbb952d72adddaaf90e9b323
risonakamo <hmykanae@gmail.com>
2018-05-01 14:59:15 -0400

    async retry on page change

--- steamtile.js
+++ steamtile.js
@@ -1,7 +1,42 @@
 document.head.insertAdjacentHTML("beforeend",`<link rel="stylesheet" href="${chrome.runtime.getURL("steamtile.css")}">`);
 
-var searchrows=document.querySelectorAll(".search_result_row");
-for (var x=0,l=searchrows.length;x<l;x++)
+var _currsearchrows;
+function replaceImages()
 {
-    searchrows[x].firstElementChild.firstElementChild.src=`https://steamcdn-a.akamaihd.net/steam/apps/${searchrows[x].href.replace(/.*\/(\d+)\/.*/,"$1")}/header.jpg`;
+    var searchrows=document.querySelectorAll(".search_result_row");
+
+    if ((_currsearchrows && searchrows[0]==_currsearchrows[0]) || searchrows.length==0)
+    {
+        setTimeout(()=>{
+            replaceImages();
+        },100);
+        return;
+    }
+
+    _currsearchrows=searchrows;
+
+    for (var x=0,l=searchrows.length;x<l;x++)
+    {
+        searchrows[x].firstElementChild.firstElementChild.src=`https://steamcdn-a.akamaihd.net/steam/apps/${searchrows[x].href.replace(/.*\/(\d+)\/.*/,"$1")}/header.jpg`;
+    }
+
+    attachButtons();
 }
+
+function attachButtons()
+{
+    var pagebuttons=document.querySelectorAll(".search_pagination_right a");
+    for (var x=0,l=pagebuttons.length;x<l;x++)
+    {
+        if (!pagebuttons[x].attached)
+        {
+            pagebuttons[x].addEventListener("click",(e)=>{
+                console.log("hey");
+                replaceImages();
+            });
+        }
+        pagebuttons[x].attached=1;
+    }
+}
+
+replaceImages();
